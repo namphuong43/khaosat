1e24a84ada9376db5c1d767e99e72506e60f06a0
namphuong43 <88895411+namphuong43@users.noreply.github.com>
2021-08-13 23:53:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,12 @@ const CONFIG = {
     introDesc : 'Em yêu anh nhiều lắm anh có biết hem' ,
     btnIntro : 'hihi' ,
     title: 'happy aniversary 5 tháng của tụi mình <3',
-    mô tả :    'Iu em hem, tha thứ cho em hem '
-    btnYes: 'Cóoo anh iu em <33',
+    desc :'Iu em hem, tha thứ cho em hem'
+    btnYes: 'Cóoo anh iu em <33' ,
     btnNo: 'hông, anh muốn thêm gì đóoo:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho en nè <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    mess: 'Em biết mà. Yêu anh nhiều nhiều',
     messDesc: 'Anh hứa bên em mãi mãi nhee',
-    btnAccept: 'Hứâaaa <3',
-    
+    btnAccept: 'Hứâaaa <3' 
